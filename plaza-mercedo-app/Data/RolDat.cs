10048cab9b16f33d3604eafae8e395f8e1f30662
marcelalizcano <linalizcanogaviria@gmail.com>
2024-10-11 14:50:11 -0500

    se termina el crud de la tabla Rol

--- plaza-mercedo-app/Data/RolDat.cs
+++ plaza-mercedo-app/Data/RolDat.cs
@@ -9,7 +9,138 @@ namespace Data
 {
     public class RolDat
     {
-        
+        // Se crea una instancia de la clase Persistence para manejar la conexión a la base de datos.
+        Persistence objPer = new Persistence();
+        public DataSet showRol()
+        {
+            // Se crea un adaptador de datos para MySQL.
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+
+            // Se crea un DataSet para almacenar los resultados de la consulta.
+            DataSet objData = new DataSet();
+
+            // Se crea un comando MySQL para seleccionar los productos utilizando un procedimiento almacenado.
+            MySqlCommand objSelectCmd = new MySqlCommand();
+
+            // Se establece la conexión del comando utilizando el método openConnection() de Persistence.
+            objSelectCmd.Connection = objPer.openConnection();
+
+            // Se especifica el nombre del procedimiento almacenado a ejecutar.
+            objSelectCmd.CommandText = "spSelectrol";
+
+            // Se indica que se trata de un procedimiento almacenado.
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Se establece el comando de selección del adaptador de datos.
+            objAdapter.SelectCommand = objSelectCmd;
+
+            // Se llena el DataSet con los resultados de la consulta.
+            objAdapter.Fill(objData);
+
+            // Se cierra la conexión después de obtener los datos.
+            objPer.closeConnection();
+
+            // Se devuelve el DataSet que contiene los productos.
+            return objData;
+        }
+        //Metodo para guardar un nuevo Rol
+        public bool saveRol(string _nombre, string _description)
+        {
+            // Se inicializa una variable para indicar si la operación se ejecutó correctamente.
+            bool executed = false;
+            int row;// Variable para almacenar el número de filas afectadas por la operación.
+
+            // Se crea un comando MySQL para insertar un nuevo Rol utilizando un procedimiento almacenado.
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spInsertRol"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Se agregan parámetros al comando para pasar los valores del rol.
+            objSelectCmd.Parameters.Add("r_nombre", MySqlDbType.VarString).Value = _nombre;
+            objSelectCmd.Parameters.Add("r_description", MySqlDbType.VarString).Value = _description;
+
+
+
+            try
+            {
+                // Se ejecuta el comando y se obtiene el número de filas afectadas.
+                row = objSelectCmd.ExecuteNonQuery();
+
+                // Si se inserta una fila correctamente, se establece executed a true.
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                // Si ocurre un error durante la ejecución del comando, se muestra en la consola.
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            // Se devuelve el valor de executed para indicar si la operación se ejecutó correctamente.
+            return executed;
+        }
+        //Metodo para actulizar un rol
+        public bool updateRol(int _id, string _nombre, string _description)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spUpdateRol"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Se agregan parámetros al comando para pasar los valores del rol.
+            objSelectCmd.Parameters.Add("r_nombre", MySqlDbType.VarString).Value = _nombre;
+            objSelectCmd.Parameters.Add("r_description", MySqlDbType.VarString).Value = _description;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+        //Metodo para borrar un rol
+        public bool deleteRol(int _idRol)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spDeleterol"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("r_id", MySqlDbType.Int32).Value = _idRol;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+
+        }
+
 
     }
 }
