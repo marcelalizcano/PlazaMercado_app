86d9dde0a1c60afe7fbd86491076a244b5b7121a
Deybison Perez <deybison2026@gmail.com>
2024-10-11 12:45:12 -0500

    Se sube el crud de la tabla pedidos

--- plaza-mercedo-app/Data/PedidosDat.cs
+++ plaza-mercedo-app/Data/PedidosDat.cs
@@ -7,5 +7,142 @@ namespace Data
 {
     public class PedidosDat
     {
+        // Se crea una instancia de la clase Persistence para manejar la conexión a la base de datos.
+        Persistence objPer = new Persistence();
+        public DataSet showPedidos()
+        {
+            // Se crea un adaptador de datos para MySQL.
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+
+            // Se crea un DataSet para almacenar los resultados de la consulta.
+            DataSet objData = new DataSet();
+
+            // Se crea un comando MySQL para seleccionar los productos utilizando un procedimiento almacenado.
+            MySqlCommand objSelectCmd = new MySqlCommand();
+
+            // Se establece la conexión del comando utilizando el método openConnection() de Persistence.
+            objSelectCmd.Connection = objPer.openConnection();
+
+            // Se especifica el nombre del procedimiento almacenado a ejecutar.
+            objSelectCmd.CommandText = "spSelectPedidos";
+
+            // Se indica que se trata de un procedimiento almacenado.
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Se establece el comando de selección del adaptador de datos.
+            objAdapter.SelectCommand = objSelectCmd;
+
+            // Se llena el DataSet con los resultados de la consulta.
+            objAdapter.Fill(objData);
+
+            // Se cierra la conexión después de obtener los datos.
+            objPer.closeConnection();
+
+            // Se devuelve el DataSet que contiene los productos.
+            return objData;
+        }
+        //Metodo para guardar un nuevo Pedidos
+        public bool savePedidos(DateTime _date, string _state, string _specification, int _fkCliente, int _fkproducto)
+        {
+            // Se inicializa una variable para indicar si la operación se ejecutó correctamente.
+            bool executed = false;
+            int row;// Variable para almacenar el número de filas afectadas por la operación.
+
+            // Se crea un comando MySQL para insertar un nuevo producto utilizando un procedimiento almacenado.
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spInsertPedidos"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Se agregan parámetros al comando para pasar los valores del producto.
+            objSelectCmd.Parameters.Add("p_date", MySqlDbType.VarString).Value = _date;
+            objSelectCmd.Parameters.Add("p_state", MySqlDbType.VarString).Value = _state;
+            objSelectCmd.Parameters.Add("p_specification", MySqlDbType.Int32).Value = _specification;
+            objSelectCmd.Parameters.Add("fkcliente", MySqlDbType.Double).Value = _fkCliente;
+            objSelectCmd.Parameters.Add("fkproducto", MySqlDbType.Int32).Value = _fkproducto;
+
+
+            try
+            {
+                // Se ejecuta el comando y se obtiene el número de filas afectadas.
+                row = objSelectCmd.ExecuteNonQuery();
+
+                // Si se inserta una fila correctamente, se establece executed a true.
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                // Si ocurre un error durante la ejecución del comando, se muestra en la consola.
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            // Se devuelve el valor de executed para indicar si la operación se ejecutó correctamente.
+            return executed;
+        }
+        //Metodo para actulizar un pedidos
+        public bool updatePedidos(int _id, DateTime _date, string _state, string _specification, int _fkCliente, int _fkproducto)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spUpdatePedidos"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Se agregan parámetros al comando para pasar los valores del producto.
+            objSelectCmd.Parameters.Add("p_id", MySqlDbType.Int32).Value = _id;
+            objSelectCmd.Parameters.Add("p_date", MySqlDbType.VarString).Value = _date;
+            objSelectCmd.Parameters.Add("p_state", MySqlDbType.VarString).Value = _state;
+            objSelectCmd.Parameters.Add("p_specification", MySqlDbType.Int32).Value = _specification;
+            objSelectCmd.Parameters.Add("fkcliente", MySqlDbType.Double).Value = _fkCliente;
+            objSelectCmd.Parameters.Add("fkproducto", MySqlDbType.Int32).Value = _fkproducto;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+        //Metodo para borrar un Pedidos
+        public bool spDeletePedidos(int _idPedidos)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spDeletePedidos"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("p_id", MySqlDbType.Int32).Value = _idPedidos;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+
+        }
     }
 }
