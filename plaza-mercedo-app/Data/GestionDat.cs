4aee0bf58e69cc9f5fad92d4f0c7168105b1bd2a
Juan Fernando Alegría <juanmonca10czm@gmail.com>
2024-10-11 16:49:21 -0500

    Se implementa el crud para la tabla gestión

--- plaza-mercedo-app/Data/GestionDat.cs
+++ plaza-mercedo-app/Data/GestionDat.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,128 @@ namespace Data
 {
     public class GestionDat
     {
+        Persistence objPer = new Persistence();
+
+        //Metodo para mostrar todas las gestiones
+        public DataSet showManagement()
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spSelectManagement";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objAdapter.SelectCommand = objSelectCmd;
+            objAdapter.Fill(objData);
+            objPer.closeConnection();
+            return objData;
+        }
+        //Metodo para mostrar unicamente por ID
+        public DataSet showManagementDDL()
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spSelectManagementDDL";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objAdapter.SelectCommand = objSelectCmd;
+            objAdapter.Fill(objData);
+            objPer.closeConnection();
+            return objData;
+        }
+
+        //Metodo para guardar una nueva gestion
+        public bool saveManagement(string p_ges_descripcion, DateTime p_ges_fecha)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spInsertManagement"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("ges_descripcion", MySqlDbType.VarString).Value = p_ges_descripcion;
+            objSelectCmd.Parameters.Add("ges_fecha", MySqlDbType.DateTime).Value = p_ges_fecha;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+
+        }
+
+        //Metodo para actualizar una gestion
+        public bool updateManagement(int p_ges_id, DateTime p_ges_fecha, string p_ges_descripcion)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spUpdateManagement"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("ges_id", MySqlDbType.Int32).Value = p_ges_id;
+            objSelectCmd.Parameters.Add("ges_fecha", MySqlDbType.DateTime).Value = p_ges_fecha;
+            objSelectCmd.Parameters.Add("ges_descripcion", MySqlDbType.String).Value = p_ges_descripcion;
+
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+
+        }
+
+        //Metodo para borrar una gestion
+        public bool deleteManagement(int p_ges_id)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spDeleteManagement"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("ges_id", MySqlDbType.Int32).Value = p_ges_id;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+
+        }
     }
 }
