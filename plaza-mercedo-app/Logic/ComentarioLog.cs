776102d7accb40ccf0ddd1ac65eed69ec0849ec4
Juan Fernando Alegría <juanmonca10czm@gmail.com>
2024-10-19 11:03:14 -0500

    Se implementa la clase comentario log

--- plaza-mercedo-app/Logic/ComentarioLog.cs
+++ plaza-mercedo-app/Logic/ComentarioLog.cs
@@ -1,11 +1,43 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
+using System.Runtime.Remoting;
 using System.Web;
 
 namespace Logic
 {
     public class ComentarioLog
     {
+        ComentarioDat objCat = new ComentarioDat();
+
+        //Metodo para mostrar todas los comentarios
+        public DataSet showComment()
+        {
+            return objCat.showComment();
+        }
+
+        //Metodo para mostrar unicamente el id y la descripcion
+        public DataSet showComentDDL()
+        {
+            return objCat.showComentDDL();
+        }
+        //Metodo para guardar un nuevo comentario
+        public bool saveComment(string p_com_text, DateTime p_com_fecha, int p_com_clasificacion)
+        {
+            return objCat.saveComment(p_com_text, p_com_fecha, p_com_clasificacion);
+        }
+        //Metodo para actualizar un comentario
+        public bool updateComment(int p_com_id, int p_com_clasificacion, string p_com_text, DateTime p_com_fecha)
+        {
+            return objCat.updateComment(p_com_id, p_com_clasificacion, p_com_text, p_com_fecha);
+        }
+        //Metodo para borrar un comentario
+        public bool deleteComment(int p_com_id)
+        {
+            return objCat.deleteCategory(p_com_id);
+        }
     }
 }
