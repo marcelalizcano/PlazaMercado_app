526939ad7f93231b2f491ff15c836a525400990e
Juan Fernando Alegría <juanmonca10czm@gmail.com>
2024-10-19 10:33:20 -0500

    Se implementa la clase categoria log

--- plaza-mercedo-app/Logic/CategoriaLog.cs
+++ plaza-mercedo-app/Logic/CategoriaLog.cs
@@ -7,5 +7,33 @@ namespace Logic
 {
     public class CategoriaLog
     {
+        CategoriaDat objCat = new CategoriaDat();
+
+        //Metodo para mostrar todas las Categorias
+        public DataSet showCategories()
+        {
+            return objCat.showCategories();
+        }
+
+        //Metodo para mostrar unicamente el id y la descripcion
+        public DataSet showCategoriesDDL()
+        {
+            return objCat.showCategoriesDDL();
+        }
+        //Metodo para guardar una nueva Categoria
+        public bool saveCategory(string _description)
+        {
+            return objCat.saveCategory(_description);
+        }
+        //Metodo para actualizar una Categoria
+        public bool updateCategory(int _idCategory, string _description)
+        {
+            return objCat.updateCategory(_idCategory, _description);
+        }
+        //Metodo para borrar una Categoria
+        public bool deleteCategory(int _idCategory)
+        {
+            return objCat.deleteCategory(_idCategory);
+        }
     }
 }
